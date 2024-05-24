3d0ae80164a8b0ddeb086dd8d3b8c48eae156f81
gnowak40 <gnowak40@stuy.edu>
2024-05-24 14:49:04 -0400

    added makefile and did a leastsigbit function

--- code/program.c
+++ code/program.c
@@ -3,7 +3,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 #include <errno.h>
+
 
 int err() {
     printf("Error %d: %s\n", errno, strerror(errno));
@@ -14,21 +16,34 @@ int err() {
 int checkWav(int fd) {
     char* s = malloc(5);
     if(read(fd, s, 4) < 4) return 0;
-    
+
 }
 
-int main() {
-    char* filePath = "test.wav";
-    int fd = open(filePath, O_RDONLY);
-    if(fd < 0) err();
-    if(!checkWav(fd)) {
-        printf("Error: Not WAVE format\n");
-        return 1;
-    }
-    unsigned char c;
-    if()
-    while(read(fd, &c, 1)) { //keep reading byte by byte into c until no more bytes
 
+//takes 'byte' and zeros out all least significant bits up to and including 'num' and inserts 'insert' into those bits.
+unsigned char leastSigBit(unsigned char byte, int num, int insert){
+  return byte - (byte % ((int) pow(2,num) + insert;
+}
+
+
+int main(int argc, char *argv[]) {
+    // char* filePath = "test.wav";
+    // int fd = open(filePath, O_RDONLY);
+    // if(fd < 0) err();
+    // if(!checkWav(fd)) {
+    //     printf("Error: Not WAVE format\n");
+    //     return 1;
+    // }
+    // unsigned char c;
+    // if()
+    // while(read(fd, &c, 1)) { //keep reading byte by byte into c until no more bytes
+    //
+    // }
+
+    if(( argc > 0 || strcmp(argv[1], "encode") == 0 ) {
+      printf("yay!\n");
     }
+    unsigned char byte = 95;
+    printf("%d\n", leastSigBit(byte, 4, 7));
     return 0;
 }
