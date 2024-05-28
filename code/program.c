af4c955a72fa3201520118c0a2c188a36f27070f
Andrew Che <ache40@stuy.edu>
2024-05-27 23:39:27 -0400

    Wrote a function to extract the LSB info from a WAV

--- code/program.c
+++ code/program.c
@@ -65,13 +65,32 @@ void LSBinsert(unsigned char* bytes, int length, char* msg) {
         printf("WAV file provided is too small to store data\n");
         exit(1);
     }
-    for(int i = 0; i < strlen(msg); ++i) {
-        int val = (int)(msg[i]);
+    for(int i = 0; i <= strlen(msg); ++i) {
+        int val;
+        if(i == strlen(msg)) val = 0;
+        else val = (int)(msg[i]);
         bytes[4*i] = leastSigBit(bytes[4*i], 2, (val & 192) >> 6);
         bytes[4*i+1] = leastSigBit(bytes[4*i+1], 2, (val & 48) >> 4);
         bytes[4*i+2] = leastSigBit(bytes[4*i+2], 2, (val & 12) >> 2);
         bytes[4*i+3] = leastSigBit(bytes[4*i+3], 2, (val & 3));
     }
+}
+
+void LSBextract(unsigned char* bytes) {
+    int i = 1, pos = 0;;
+    while(i) {
+        i = 0;
+        i += (int)(bytes[pos]) & 3;
+        i = i << 2;
+        i += (int)(bytes[pos+1]) & 3;
+        i = i << 2;
+        i += (int)(bytes[pos+2]) & 3;
+        i = i << 2;
+        i += (int)(bytes[pos+3]) & 3;
+        printf("%c", i);
+        pos += 4;
+    }
+    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -106,5 +125,13 @@ int main(int argc, char* argv[]) {
     write(fdOut, buff, dataSize);
     close(fd);
     close(fdOut);
+    int fdNew = open(argv[2], O_RDONLY);
+    if(fdNew < 0) err();
+    while( (readBytes = read(fdNew, buff, 4)) ){
+        if(strncmp(buff, "data", 4) == 0) break;
+    }
+    read(fdNew, buff, 4);
+    read(fdNew, bytes, dataSize);
+    LSBextract(bytes);
     //drawGraph(bytes, dataSize);
 }
